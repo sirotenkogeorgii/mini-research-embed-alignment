12110387493a8630d902f6312d1a9686c9370913
georgiisirotenko <sirotenkoegor2001@yandex.com>
2025-03-21 00:06:49 +0100

    fix vision extraction

--- src/rep_extractor.py
+++ src/rep_extractor.py
@@ -12,72 +12,44 @@ from src.utils.file_saver import FileSaver
 class RepExtractor:
     def __init__(self, config: DictConfig) -> None:
         self.config: DictConfig = config
-        self.dataset_name: str = config.dataset.dataset_name
 
-
-        # TODO: Uncomment and fix when I will do vision part
-        # self.image_id_pairs: str = config.dataset.image_id_pairs
-
-
-        self.model_name: str = config.model.model_name
+        # self.model_name: str = config.model.model_name
         self.model_type: ModelType = config.model.model_type
-        self.model_dim: int = config.model.dim
-        self.seed: int = config.common.seed
-        self.current_language: str = config.muse.language
+        # self.model_dim: int = config.model.dim
+        # self.seed: int = config.common.seed
+        # self.current_language: str = config.muse.language
         
         self.file_saver = FileSaver(self.config)
 
         self.config.common.embeddings_dataset_root = f"{config.common.embeddings_dataset_root}/{self.model_type.value}"
+        # self.config.common.embeddings_dataset_root = f"{config.common.embeddings_dataset_root}/{config.model.model_type.value}"
         
-        # Layer selection configuration
         
-        # Embedding storage options
-        self.emb_per_object: bool = config.common.emb_per_object
-
     def process_embeddings(self) -> None:
         if self.model_type == ModelType.VM:
-        # TODO: Uncomment and fix when I will do vision part
-            # self.__process_vision_embeddings()
+            self.__process_vision_embeddings()
             return
         elif self.model_type == ModelType.LM:
             self.__process_language_embeddings()
 
-# TODO: Uncomment and fix when I will do vision part
-    # def __process_vision_embeddings(self) -> None:
-    #     """Process vision embeddings for all languages - same images for all languages"""
-    #     self.alias_emb_dir = self.alias_emb_dir / self.dataset_name
-    #     if not self.alias_emb_dir.exists():
-    #         self.alias_emb_dir.mkdir(parents=True, exist_ok=True)
+    def __process_vision_embeddings(self) -> None:
+        """Process vision embeddings for all languages - same images for all languages"""
+        # with open(self.config.dataset.vision_data.image_id_pairs, "r") as f:
+        with open(self.config.dataset.vision_data.available_image_ids, "r") as file:
+            # image id pairs are "n01983481": ["american_lobster", "maine_lobster"]
+            # image_id_pairs = json.load(f)
+            # image_ids = [i for i in list(image_id_pairs.keys())] # n04357314 like keys
+            lines = file.readlines()
+            image_ids = [line.strip() for line in lines]
 
-    #     with open(self.image_id_pairs, "r") as f:
-    #         image_id_pairs = json.load(f)
-    #         image_ids = [i for i in list(image_id_pairs.keys())]
-
-    #     save_file_path = self.alias_emb_dir / f"{self.model_name}_{self.model_dim}.pth"
-    #     if save_file_path.exists():
-    #         print(f"File {save_file_path} already exists.")
-    #         return
-            
-    #     self.__get_vm_rep(image_ids)
+        self.__get_vm_rep(image_ids)
 
     def __process_language_embeddings(self) -> None:
         """Process language embeddings for the current language"""
-        # Create language-specific directory
-        # lang_dir = self.alias_emb_dir / self.current_language
-        # if not lang_dir.exists():
-        #     lang_dir.mkdir(parents=True, exist_ok=True)
-
-        # save_file_path = lang_dir / f"{self.model_name}_{self.model_dim}.pth"
-        # # TODO: add an option to override if exists
-        # if save_file_path.exists():
-        #     print(f"File {save_file_path} already exists.")
-        #     return
-            
         self.__get_lm_rep()
 
     def __get_vm_rep(self, image_labels: list) -> None:
-        # TODO: rename to ...Extractor instead of ...Embedding.
-        embeddings_extractor = VMEmbedding(self.config, image_labels)
+        embeddings_extractor = VMEmbedding(self.config, image_labels, self.file_saver)
         embeddings_extractor.get_vm_layer_representations()
 
     def __get_lm_rep(self) -> None:
